cfe5aa2e1ad229db8726a9a09efaddcdd68b9fcc
Ross Blanchard <ross.blanchard@helix.com>
2018-09-26 14:37:27 -0700

    applying app factoring pattern

--- services/genes/project/__init__.py
+++ services/genes/project/__init__.py
@@ -1,51 +1,33 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-# instantiate the app
-app = Flask(__name__)
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
 # instantiate the db
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-# model
-class Variants(db.Model):
-    __tablename__ = 'variants'
-    id = db.Column(db.Integer, primary_key=True)
-    gene = db.Column(db.String(50))
-    nucleotide_change = db.Column(db.String(150))
-    protein_change = db.Column(db.String(100))
-    other_mappings = db.Column(db.String(100))
-    alias = db.Column(db.String(100))
-    transcripts = db.Column(db.String())
-    region = db.Column(db.String(50))
-    reported_classification = db.Column(db.String(100))
-    inferred_classification = db.Column(db.String(100))
-    source = db.Column(db.String(100))
-    last_evaluated = db.Column(db.Date())
-    last_updated = db.Column(db.Date())
-    url = db.Column(db.String(100))
-    submitter_comment = db.Column(db.String())
-    assembly = db.Column(db.String(15))
-    chr = db.Column(db.Integer())
-    genomic_start = db.Column(db.Integer())
-    genomic_stop = db.Column(db.Integer())
-    ref = db.Column(db.String(100))
-    alt = db.Column(db.String(100))
-    accession = db.Column(db.String(25))
-    reported_ref = db.Column(db.String(100))
-    reported_alt = db.Column(db.String(100))
+def create_app(script_info=None):
 
+    # instantiate the app
+    app = Flask(__name__)
 
-@app.route('/genes/ping', methods=['GET'])
-def get_gene():
-    return jsonify({
-        'gene': 'RHD',
-        'nucleotide_changes': 'NM_000789.3:c.2306-117_2306-116insAF118569.1:g.14094_14382'
-    })
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set extensions
+    db.init_app(app)
+
+    # register endpoints
+    from project.api.genes import genes_blueprint
+    app.register_blueprint(genes_blueprint)
+
+    # registering app and db to the shell, avoiding direct importing
+    @app.shell_context_processor
+    def ctx():
+        return {
+            'app': app,
+            'db': db
+        }
+    return app
