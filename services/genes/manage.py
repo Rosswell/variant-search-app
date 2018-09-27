78b82629192cfc93e23dfee465db243865369af4
Ross Blanchard <ross.blanchard@helix.com>
2018-09-26 23:34:49 -0700

    add code coverage functionality and linting

--- services/genes/manage.py
+++ services/genes/manage.py
@@ -2,7 +2,18 @@ from flask.cli import FlaskGroup
 from project import create_app, db
 from project.api.models import Variant
 import unittest
+import coverage
 
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -10,6 +21,7 @@ cli = FlaskGroup(create_app=create_app)
 
 @cli.command()
 def recreate_db():
+    """ Recreate all the DBs """
     db.drop_all()
     db.create_all()
     db.session.commit()
@@ -17,12 +29,34 @@ def recreate_db():
 
 @cli.command()
 def test():
-    """ Runs the tests without code coverage"""
+    """ Runs the tests without code coverage """
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
     return 1
+
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
+
+@cli.command()
+def ingest_from_tsv():
+    """ Load prod data from tsv """
+    # TODO: do this (requires preprocessing)
 
 
 if __name__ == '__main__':
