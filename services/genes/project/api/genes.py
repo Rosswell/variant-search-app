b446e5e8599f6793192f1164581648e8320f88d3
Ross Blanchard <ross.blanchard@helix.com>
2018-09-26 18:05:18 -0700

    add multi-variant response

--- services/genes/project/api/genes.py
+++ services/genes/project/api/genes.py
@@ -1,12 +1,28 @@
 from flask import Blueprint, jsonify
-
+from project.api.models import Variant
 
 genes_blueprint = Blueprint('genes', __name__)
 
 
 @genes_blueprint.route('/genes/ping', methods=['GET'])
-def get_gene():
+def ping_gene():
     return jsonify({
         'gene'              : 'RHD',
         'nucleotide_changes': 'NM_000789.3:c.2306-117_2306-116insAF118569.1:g.14094_14382'
     })
+
+
+@genes_blueprint.route('/genes/<gene_id>', methods=['GET'])
+def get_genes(gene_id):
+    """
+    GET the details for a single gene. This will likely result in multiple results due to the potential number of
+    variants in a single gene
+    """
+
+    response_object = {
+        'status': 'success',
+        'data': {
+            'variants': [variant.to_json() for variant in Variant.query.filter_by(gene=gene_id).all()]
+        }
+    }
+    return jsonify(response_object), 200
--- services/genes/project/tests/test_genes.py
+++ services/genes/project/tests/test_genes.py
@@ -1,6 +1,19 @@
 import json
 import unittest
 from project.tests.base import BaseTestCase
+from project.api.models import Variant
+from project.tests.data import multi_variant_data
+from project import db
+
+
+def add_variants(gene_dicts):
+    variant_objs = []
+    for variant in gene_dicts:
+        variant_obj = Variant(**variant)
+        variant_objs.append(variant_obj)
+        db.session.add(variant_obj)
+        db.session.commit()
+    return variant_objs
 
 
 class TestGeneService(BaseTestCase):
@@ -8,11 +21,23 @@ class TestGeneService(BaseTestCase):
 
     def test_genes(self):
         """Ensure the /ping route behaves correctly."""
+
         response = self.client.get('/genes/ping')
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
         self.assertIn('RHD', data['gene'])
         self.assertIn('NM_000789.3:c.2306-117_2306-116insAF118569.1:g.14094_14382', data['nucleotide_changes'])
+
+    def test_single_gene(self):
+        """ Ensure get single gene behaves correctly."""
+        genes = add_variants(multi_variant_data['data'])
+        with self.client:
+            response = self.client.get('/genes/{}'.format(multi_variant_data['data'][0]['gene']))
+            data = json.loads(response.data.decode())
+            # print(data)
+            self.assert200(response)
+            self.assertEqual(multi_variant_data['data'][0]['gene'], data['data']['variants'][0]['gene'])
+            self.assertEqual('success', data['status'])
 
 
 if __name__ == '__main__':
