b3c3875c6af7bd8cfd6682f42d3f102bbddec448
Ross Blanchard <ross.blanchard@helix.com>
2018-09-27 22:10:05 -0700

    customize column styling, split into multiline

--- services/client/src/components/GeneTable.js
+++ services/client/src/components/GeneTable.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import ReactTable from 'react-table';
 import "react-table/react-table.css";
-import axios from 'axios';
+// import axios from 'axios';
 
 
 const getColumns = (data) => {
   const columns = [];
   const sample = data[0];
   for (let key in sample) {
-    if (key === "gene") {
-      columns.push({
-        accessor: key,
-        Header: key,
-        pivot: true
-      });
-    } else {
-      columns.push({
-        accessor: key,
-        Header: key
-      });
+    let columnFields = {
+      accessor: key,
+      Header: key.replace('_', ' '),
+      headerClassName: 'gene-table__header',
+      className: 'gene-table__column',
+      minWidth: 175
     };
+    switch (key) {
+      case 'nucleotide_change':
+        columnFields.minWidth = 450;
+        break;
+      case ('other_mappings' || 'transcripts'):
+          columnFields.minWidth = 450;
+          columnFields.className = 'gene-table__list-column'
+          columnFields.Cell = row => (
+            <div>
+              {
+                row.value ? row.value.replace(new RegExp(',', 'g'), ' ') : row.value
+              }
+            </div>
+          );
+          break;
+      case 'url':
+        columnFields.Cell = row => (
+          <a href={row.value}>NCBI Reference</a>
+        );
+        break;
+      case 'submitter_comment':
+        columnFields.minWidth = 450;
+        break;
+      default:
+        break;
+    }
+    columns.push(columnFields);
   }
   return columns;
 }
@@ -27,51 +49,42 @@ const getColumns = (data) => {
 
 export default class GeneTable extends React.Component {
   state = {
-    pages: null,
-    loading: true
+    // pages: this.props.data.length,
+    loading: true,
+    pageSize: 0
   };
-
-  // handleSearchGene = (option) => {
-  //   if (!this.state.geneInput) {
-  //     return 'Enter valid gene to search for'
-  //   } else if (this.state.data.indexOf(option) > -1) {
-  //     return 'This option already exists'
-  //   }
-
-  //   this.setState((prevState) => ({
-  //     options: prevState.options.concat(option)
-  //   }))
-  // };
-
-  // fetchData = (searchedGene) => {
-  //   // show the loading overlay
-  //   this.setState({loading: true})
-  //   // fetch data
-  //   axios.get(`${process.env.REACT_APP_GENES_SERVICE_URL}:5001/genes/${searchedGene}`)
-  //       .then((res) => { 
-  //         console.log(res);
-  //         this.setState({
-  //           data: res.data.data.variants,
-  //           loading: false
-  //         });
-  //       }).catch((err) => { console.log(err); });
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.data !== this.props.data) {
+      this.setState({pageSize:this.props.data.length})
+    }
+    // console.log(this.props.data.length)
+    // this.setState({pageSize:this.props.data.length})
+  }
+  // componentDidReceiveProps() {
+  //   console.log(this.props.data.length)
+  //   this.setState({pageSize:this.props.data.length})
   // }
-
+  // comp
   render() {
+    // const columns = getColumns(this.props.data)
+    // console.log(this.props.data.length)
     return(
       <div>
         <ReactTable
-          // data={this.state.data}
           data={this.props.data}
           columns={getColumns(this.props.data)}
-          // pivotBy={["gene"]}
-          pages={this.state.pages}
+          defaultSorted={[
+            {
+              id: "genomic_start",
+              desc: false
+            }
+          ]}
+          // pages={this.state.pages}
           loading={this.props.loading}
-          // sorting and filtering handled server-side 
-          // TODO: figure out where to do this most efficiently. I would guess server but maybe not
-          manual
-          // 
-          onFetchData={(state, instance) => {this.props.fetchData(this.props.geneInput)}}
+          defaultPageSize={this.state.pageSize}
+          noDataText='No genes found with given name'
+          // onFetchData={() => {this.props.fetchData(this.props.geneInput)}}
+          className="gene-table"
         />
       </div>
     )
